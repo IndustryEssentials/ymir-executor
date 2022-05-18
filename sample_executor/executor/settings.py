97cb96a47d9d9bd866d1db7dd1ce6233d3fb92d7
youdaoyzbx <youdaoyzbx@163.com>
2022-05-18 17:10:53 +0800

    remove sample_executor

--- sample_executor/executor/settings.py
+++
@@ -1,5 +0,0 @@
-
-# default location of env yaml file
-# if your app is running inside docker image, env file is always at /in/env.yaml
-# you can change the location of this file IF AND ONLY IF you are doing test
-DEFAULT_ENV_FILE_PATH = '/in/env.yaml'
