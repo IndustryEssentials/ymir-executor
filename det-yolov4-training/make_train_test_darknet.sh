c85f6ea87780aa4222c937eba9a927ae0d5136f1
jingyiliu <liu.jingyi@intellif.com>
2022-05-26 17:39:38 +0800

    make paths configurable for det-yolov4-training

--- det-yolov4-training/make_train_test_darknet.sh
+++ det-yolov4-training/make_train_test_darknet.sh
@@ -1,10 +1,13 @@
 set -e
 
-mkdir -p /out/models
+in="${1:-/in}"
+out="${2:-/out}"
+
+mkdir -p ${out}/models
 
 # update monitor, state: running
-cat /in/config.yaml | grep task_id: | tr -s " " "\012" > /out/tmp.txt
-echo -e "$(sed '2!d' /out/tmp.txt)\t$(date +%s).000000\t"0.00"\t"2"\t" > /out/monitor.txt
+cat ${in}/config.yaml | grep task_id: | tr -s " " "\012" > ${out}/tmp.txt
+echo -e "$(sed '2!d' ${out}/tmp.txt)\t$(date +%s).000000\t"0.00"\t"2"\t" > ${out}/monitor.txt
 
 # convert label format from ark format to darknet need to pay attention here the code "convert_label_ark2txt.py" fit for 1 class
 python3 ./convert_label_ark2txt.py
@@ -15,4 +18,4 @@ python3 ./convert_label_ark2txt.py
 python3 config_and_train.py
 
 # update monitor, state: running
-echo -e "$(sed '2!d' /out/tmp.txt)\t$(date +%s).000000\t"1.0"\t"2"\t" > /out/monitor.txt
+echo -e "$(sed '2!d' ${out}/tmp.txt)\t$(date +%s).000000\t"1.0"\t"2"\t" > ${out}/monitor.txt
