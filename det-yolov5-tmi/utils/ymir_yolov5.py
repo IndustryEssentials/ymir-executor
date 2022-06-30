8a01ee0c7e57c34bdfbb51f42e70ef3e83df0e59
youdaoyzbx <youdaoyzbx@163.com>
2022-06-30 18:03:50 +0800

    fix spell error

--- det-yolov5-tmi/utils/ymir_yolov5.py
+++ det-yolov5-tmi/utils/ymir_yolov5.py
@@ -70,7 +70,7 @@ def get_weight_file(cfg: edict) -> str:
     find weight file in cfg.param.model_params_path or cfg.param.model_params_path
     """
     if cfg.ymir.run_training:
-        model_params_path = cfg.param.pretrained_model_paths
+        model_params_path = cfg.param.get('pretrained_model_params',[])
     else:
         model_params_path = cfg.param.model_params_path
 
