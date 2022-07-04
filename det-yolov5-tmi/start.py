94cefe7cf41736ce180ce7b04120b89fc421bf27
youdaoyzbx <youdaoyzbx@163.com>
2022-07-04 16:51:41 +0800

    training or mining && infer

--- det-yolov5-tmi/start.py
+++ det-yolov5-tmi/start.py
@@ -22,10 +22,11 @@ def start() -> int:
 
     if cfg.ymir.run_training:
         _run_training(cfg)
-    if cfg.ymir.run_mining:
-        _run_mining(cfg)
-    if cfg.ymir.run_infer:
-        _run_infer(cfg)
+    else:
+        if cfg.ymir.run_mining:
+            _run_mining(cfg)
+        if cfg.ymir.run_infer:
+            _run_infer(cfg)
 
     return 0
 
